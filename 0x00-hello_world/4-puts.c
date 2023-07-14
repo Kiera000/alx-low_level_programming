c15f40e3d4a7b342dc47f43cb70fa9c1bf729553
kaoutar bouabadi <bouabadi.kaoutar.05@gmail.com>
2023-07-13 18:20:13 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - A programe that prints a line with puts funtion
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+puts(*\"Programming is like building a multilingual puzzle*);
+return (0);
+}
